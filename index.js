2e7ff0d8b238968e60c0e468f855b97930be64b1
Anastasia Chen <anastasia@Anastasias-MacBook-Air.local>
2021-02-13 10:10:44 +1100

    add get route and change ejs template to display all schedules

--- index.js
+++ index.js
@@ -21,21 +21,34 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // Get list of schedules (home route)
-app.get('/', (req, res) => res.render('index', { schedules: data.schedules }));
+app.get('/', (req, res) => {
+  database.any('SELECT * from schedule;')
+  .then((schedules) => {
+    console.log(schedules)
+    res.render('pages/index', {schedules: schedules});
+  })
+  .catch((err) => {
+    console.error(err)
+    res.render('pages/error', {
+      err: err
+    })
+  })
+console.log('i happened')
+});
 
 // Display form for new schedule
-app.get('/new', (req, res) => res.render('new_schedule', { users: data.users }));
+// app.get('/new', (req, res) => res.render('new_schedule', { users: data.users }));
 
 // Post new schedule
-app.post('/schedules', (req, res) => {
-  const newSchedule = {
-    'user_id': Number(req.body.user_id),
-    'day': Number(req.body.day),
-    'start_at': req.body.start_at,
-    'end_at': req.body.end_at
-  };
-  data.schedules.push(newSchedule);
-  res.render('content_schedules', { schedules: data.schedules });
-});
+// app.post('/', (req, res) => {
+//   const newSchedule = {
+//     'user_id': Number(req.body.user_id),
+//     'day': Number(req.body.day),
+//     'start_at': req.body.start_at,
+//     'end_at': req.body.end_at
+//   };
+//   data.schedules.push(newSchedule);
+//   res.render('content_schedules', { schedules: data.schedules });
+// });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
--- sql/seed_tables.sql
+++ sql/seed_tables.sql
@@ -1,8 +1,11 @@
 INSERT INTO schedule(username, day, start_time, end_time)
-VALUES ('anastasia', 1, '09:00:00', '10:00;:00');
+VALUES ('anastasia', 1, '09:00:00', '10:00:00');
 
 INSERT INTO schedule(username, day, start_time, end_time)
-VALUES ('anastasia', 2, '10:00:00', '11:00;:00');
+VALUES ('ivan', 2, '10:00:00', '11:00:00');
 
 INSERT INTO schedule(username, day, start_time, end_time)
-VALUES ('anastasia', 3, '11:00:00', '12:00;:00');
+VALUES ('ilya', 4, '15:00:00', '22:00:00');
+
+INSERT INTO schedule(username, day, start_time, end_time)
+VALUES ('alina', 3, '11:00:00', '12:00:00');
