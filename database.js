03f738260081f50474ef8a6f1def65f00796b2f8
Anastasia Chen <anastasia@Anastasias-MacBook-Air.local>
2021-02-20 21:52:54 +1100

    add database protection with password

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const connection = 'postgres://anastasia@localhost:5432/mr_coffee_app';
+const connection = 'postgres://mr_coffee:coffee@localhost:5432/mr_coffee_app';
 
 const database = pgp(connection);
 
